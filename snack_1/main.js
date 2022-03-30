f26edc47d74f9be33473853d808cd4629f9a93fa
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-30 10:21:39 +0200

    Initial Commit

---
+++ snack_1/main.js
@@ -0,0 +1,9 @@
+let number_1 = parseInt(prompt("scrivi un numero"))
+let number_2 = parseInt(prompt("scrivi un altro numero"))
+
+
+if (number_1 > number_2){
+    console.log(number_1)
+} else{
+    console.log(number_2)
+}
