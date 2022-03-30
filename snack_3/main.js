21f3f35d114aede17875d09a731ad2b83aad18b8
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-30 11:54:35 +0200

    Initial Commit

---
+++ snack_3/main.js
@@ -0,0 +1,10 @@
+var somma = 0
+for (i=1; i<=10; i++){
+    let inserisciNumero = parseInt(prompt("Inserisci un numero"))
+    console.log(inserisciNumero)
+    somma = somma + inserisciNumero
+}
+
+console.log(somma)
+
+
