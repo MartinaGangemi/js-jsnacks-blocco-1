8e51916134604239937e378e364c122e6142008a
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-30 10:30:39 +0200

    aggiunta condizione

--- snack_1/main.js
+++ snack_1/main.js
@@ -4,6 +4,9 @@ let number_2 = parseInt(prompt("scrivi un altro numero"))
 
 if (number_1 > number_2){
     console.log(number_1)
-} else{
+} else if (number_1 < number_2){
     console.log(number_2)
+} else{
+    console.log("sono uguali")
 }
+
